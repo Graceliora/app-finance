83aac2b42ac28c952cc2352d795fd1315a56704f
Graceliora <noveliora.gracella@gmail.com>
2025-01-31 17:16:33 +0700

    Section 24: Done

--- middleware.js
+++ middleware.js
@@ -1,8 +1,21 @@
-import { NextResponse } from 'next/server'
+import { createClient } from './lib/supabase/server'
 import { updateSession } from './lib/supabase/middleware'
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request) {
+  const { data: { user }} = await createClient().auth.getUser()
+
+  // Redirect to login if no user is logged in and trying to access /dashboard
+  if (!user && request.nextUrl.pathname.startsWith('/dashboard')) {
+    return Response.redirect(new URL('/login', request.url))
+  }
+
+  // Redirect to dashboard if user is logged in and trying to access /login
+  if (user && request.nextUrl.pathname === '/login') {
+    return Response.redirect(new URL('/dashboard', request.url))
+  }
+  
+  // Continue to update the session for other requests
   return await updateSession(request)
 }
 
